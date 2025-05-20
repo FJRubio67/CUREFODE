2dcf5c7689b2032aba454d830cf6614067549187
Francisco Javier Rubio <fxrubio@gmail.com>
2025-05-20 13:57:23 +0100

    Update forcedlogis.R

--- forcedlogis.R
+++ forcedlogis.R
@@ -3,6 +3,7 @@
 flogis <- function(t, y, par) {
   # state variables
   CH <- y[1]
+
   
   # parameters
   lambda <- par[1]
@@ -12,7 +13,7 @@ flogis <- function(t, y, par) {
   
   # model equations
   dCH <-  lambda*CH*(1 - CH/kappa) + eps/(1+t)
-
+  
   
   # result
   return( list(c(dCH)) )
@@ -25,7 +26,7 @@ lambda0 = 5
 kappa0 = 1
 eps0 = 0.01
 
-tvec = seq(0,5,by = 0.1)
+tvec = seq(0,5,by = 0.01)
 
 
   params  <- c(lambda = lambda0, kappa = kappa0, eps = eps0)
@@ -35,5 +36,30 @@ tvec = seq(0,5,by = 0.1)
   
   out <- ode(init, tvec, flogis, params, method = "lsode")
   
+  
+  # Get the solution for H(t)
+  H_values <- out[, 2]
+  
+  # Function to extract H'(t) after solving the ODE
+  get_derivatives <- function(times, y, func, parms) {
+    n <- length(times)
+    derivs <- numeric(n)
+    
+    for (i in 1:n) {
+      # Calculate derivative at each time point
+      derivs[i] <- unlist(func(times[i], y[i], parms))
+    }
+    
+    return(derivs)
+  }
+  
+  # Extract the derivatives (h(t) = H'(t))
+  h_values <- get_derivatives(tvec, H_values, flogis, params)
+  
 
-  plot(tvec,exp(-out[,2]), type = "l", lwd = 2, ylim = c(0,1))
+  
+  plot(tvec,exp(-H_values), type = "l", lwd = 2, ylim = c(0,1))
+
+  
+  plot(tvec,h_values, type = "l", lwd = 2, ylim = c(0,1.5))
+  
